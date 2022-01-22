a83a0b3f5a4697e29ad401cc465e2532269f27b8
Gonzalo FU <GFU-code@protonmail.ch>
2022-01-22 18:03:47 -0300

    section05 - definición de los objetos tarea y tareas que hace uso la aplicación, se agrego el método crear tareas y se bosquejo el posible método para listar las tareas creadas. Los datos aún no son almacenados de forma permanente

--- section05 - app consola/app.js
+++ section05 - app consola/app.js
@@ -1,14 +1,28 @@
 require("colors");
 
 // importando nuestros propios módulos
-const { inquirerMenu, pausa } = require("./helpers/inquirer");
+const { inquirerMenu, pausa, leerInput } = require("./helpers/inquirer");
+const Tareas = require("./models/tareas");
 
 console.clear();
 
 const main = async () => {
   let opt = "";
+  const tareas = new Tareas();
+
   do {
     opt = await inquirerMenu();
+
+    switch (opt) {
+      case "1":
+        const desc = await leerInput("Descripción de la tarea: ");
+        tareas.crearTarea(desc);
+        break;
+      case "2":
+        console.log(tareas._listado);
+        break;
+    }
+
     await pausa();
   } while (opt !== "0");
 };
