b41d9aae3d19a034046346bd4ccc18d8c9bceabb
Gonzalo FU <GFU-code@protonmail.ch>
2022-02-08 11:48:22 -0300

    section07 - configuración del middleare por defecto para servir archivos estáticos. Se aprendio la importancia en el orden de los middlewares definidos en express, ya que estos siguen una secuencia lineal.

--- section07 - webserver/app.js
+++ section07 - webserver/app.js
@@ -1,12 +1,26 @@
+// express es un módulo enfocado en la creación de servidores webs.
 const express = require("express");
+// Creamos una aplicación express (servidor)
 const app = express();
 
 const port = 8080;
 
+// Middleware: Son callbacks que vienen a complementar y modificar la cadena lógica, todo el proceso desde que se recibe una solicitud hasta que se le da respuesta, de nuestro servidor.
+
+// Middleware para servir contenido estático
+app.use(express.static("public"));
+
+// Asociamos la ruta "/" de nuestro servidor a un callback y su lógica.
 app.get("/", (req, res) => {
   res.send("Hola Mundo");
 });
 
+// Capturamos toda petición que no haya coincido con una ruta de nuestro servidor.
+app.get("*", (req, res) => {
+  res.sendFile(__dirname + "/public/404.html");
+});
+
+// Definimos el puerto por el cuál recibiremos y escucharemos las solicitudes del cliente.
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
